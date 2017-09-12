c31b6136755c3e6bcff1a6131799216cb46ee191
Elijah Sora Kim <SoraKayy@users.noreply.github.com>
2017-09-11 20:13:37 -0500

    Update if statement quadratic formula.cpp

--- if statement quadratic formula.cpp
+++ if statement quadratic formula.cpp
@@ -8,26 +8,26 @@ int main()
 	double a;
 	double b;
 	double c;
-
-	cout << "Solving a(x)^2+ b(x) + c = 0 \n";
+		
+	cout << "Solving a(x)^2+ b(x) + c = 0 \n"; //title
 	cout << "_____________________________ \n";
 
-	cout << "Please enter 'a': ";
+	cout << "Please enter 'a': "; //variable a
 	cin >> a;
 	cout << "\n";
 
-	cout << "Please enter 'b': ";
+	cout << "Please enter 'b': "; //variable b
 	cin >> b;
 	cout << "\n";
 
-	cout << "Please enter 'c': ";
+	cout << "Please enter 'c': "; //variable c
 	cin >> c;
 	cout << "\n";
 
-	double ans = (-b + sqrt(pow(b, 2) - (4 * a * c))) / 2;
-	double ans2 = (-b - sqrt(pow(b, 2) - (4 * a * c))) / 2;
+	double ans = (-b + sqrt(pow(b, 2) - (4 * a * c))) / (2 * a); //calculating with '+'
+	double ans2 = (-b - sqrt(pow(b, 2) - (4 * a * c))) / (2 * a); //calculating with '-'
 
-	if (ans || ans2 < 0)
+	if (ans && ans2 < 0) //whether to print
 		cout << "Error, there are no solutions \n";
 	else
 		cout << "There is a solution: " << ans << " and " << ans2 << "\n";
